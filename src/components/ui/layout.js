0628500c2719ebeb8d9d48088ba1c76cc800837c
Sean <>
2021-09-23 12:02:30 -0700

    almost completed section 4

--- src/components/ui/layout.js
+++ src/components/ui/layout.js
@@ -9,6 +9,7 @@ import * as React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
+import Footer from './footer'
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -36,16 +37,8 @@ const Layout = ({ children }) => {
         }}
       >
         <main>{children}</main>
-        {/* <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer> */}
       </div>
+      <Footer />
     </>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,20 @@
-import * as React from 'react'
+import React from 'react'
 import { Link } from 'gatsby'
-import { StaticImage } from 'gatsby-plugin-image'
 
 import Layout from '../components/ui/layout'
-import Seo from '../components/ui/seo'
+import HeroBlock from '../components/home/HeroBlock'
+// import PromotionalProducts from "../components/home/PromotionalProducts"
+import FeaturedProducts from '../components/home/FeaturedProducts'
+import MarketingButtons from '../components/home/MarketingButtons'
+import CallToAction from '../components/home/CallToAction'
 
 const IndexPage = () => (
   <Layout>
-
-
-
-
-
-
+    <HeroBlock />
+    {/* <PromotionalProducts /> */}
+    <FeaturedProducts />
+    <MarketingButtons />
+    <CallToAction />
   </Layout>
 )
 
