9e7fa81743a1acf6f744cb6969763bb8d4fc3a14
Sean <>
2021-09-27 23:32:40 -0700

    needing help

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,31 +2,57 @@ require('dotenv').config()
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `VAR-X`,
+    description: `The premier developer clothing line. By developers, for developers. High quality, custom-designed shirts, hats, and hoodies.`,
+    author: `Zachary Reece`,
+    keywords: [
+      'clothing',
+      'developer',
+      'programmer',
+      'coding',
+      'code',
+      'websites',
+      'web developer',
+      'hats',
+      'shirts',
+      'hoodies',
+    ],
+    siteUrl: 'https://formstorm.design',
+    twitterUsername: '@zacharydreece',
+    defaultImage: '',
   },
   plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-material-ui`,
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-plugin-robots-txt`,
+      options: {
+        host: 'https://formstorm.design',
+        sitemap: 'https://formstorm.design/sitemap.xml',
+        policy: [{ userAgent: '*', allow: '/' }],
+      },
+    },
     {
       resolve: 'gatsby-plugin-web-font-loader',
       options: {
         google: {
-          families: ['Poppins', 'Montserrat '],
+          families: [
+            'Philosopher:700:latin',
+            'Montserrat:700,600,500,400,300:latin',
+          ],
         },
       },
     },
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: `http://localhost:1337`,
-        queryLimit: 1000, // Defaults to 100
+        apiURL: process.env.GATSBY_STRAPI_URL,
+        queryLimit: 1000, // Default to 100
         collectionTypes: [`product`, `category`, `variant`],
       },
     },
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-material-ui`,
-    `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -35,23 +61,28 @@ module.exports = {
       },
     },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        defaults: {
+          formats: [`auto`, `webp`],
+          placeholder: 'blurred',
+          breakpoints: [300, 600, 960, 1280, 1920],
+        },
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `VAR-X`,
+        short_name: `VAR-X`,
         start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        background_color: `#99B898`,
+        theme_color: `#99B898`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
